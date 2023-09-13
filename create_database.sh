ab080375c3a395912edff69ce04c12440c883e77
SorinoSSK <sweeapplemage@hotmail.com>
2023-09-13 20:55:23 +0800

    Remove Backup

--- create_database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Creating SENSOR DATABASE"
-sqlite3 -echo db_sensordata.db <db_sensordata.sql
-sqlite3 -echo db_statusdata.db <db_statusdata.sql
--- db_statusdata.sql
+++
@@ -1,8 +0,0 @@
-PRAGMA foreign_keys=OFF;
-
-DROP TABLE IF EXISTS errorLog;
-CREATE TABLE errorLog (
-timestamp  INTEGER,
-event  TEXT NOT NULL
-);
-
